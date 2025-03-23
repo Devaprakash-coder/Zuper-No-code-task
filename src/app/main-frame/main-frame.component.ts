d79d22797dffc39fdc37b54a055829a61dc5cf00
Devaprakash-coder <devaprakash.m1997@gmail.com>
2025-03-23 09:19:24 +0530

    form changes

--- src/app/main-frame/main-frame.component.ts
+++ src/app/main-frame/main-frame.component.ts
@@ -1,7 +1,140 @@
+// import { Component, EventEmitter, Output, ViewChild } from '@angular/core';
+// import { CommonService } from '../common.service';
+// import { CommonModule } from '@angular/common';
+// import { FormArray, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+// import { MatIconModule } from '@angular/material/icon';
+// import { DragDropModule, CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
+// import { MatDrawer } from '@angular/material/sidenav';
+// import { MatSidenavModule } from '@angular/material/sidenav';
+// import { LeftPaneComponent } from '../left-pane/left-pane.component';
+// import { MatDialog } from '@angular/material/dialog';
+// import { ConfirmationPopupComponent } from '../confirmation-popup/confirmation-popup.component';
+
+// @Component({
+//   selector: 'app-main-frame',
+//   standalone: true,
+//   imports: [
+//     CommonModule,
+//     FormsModule,
+//     MatIconModule,
+//     DragDropModule,
+//     MatSidenavModule,
+//     LeftPaneComponent,
+//     ReactiveFormsModule
+//   ],
+//   templateUrl: './main-frame.component.html',
+//   styleUrls: ['./main-frame.component.css']
+// })
+// export class MainFrameComponent {
+//   // selectedGroup: any;
+//   editEnabled: boolean = false;
+//   selectedGroup!: FormGroup | null;
+//   formElements!: FormArray;
+
+//   formElementsList = [
+//     { name: 'Text Field', type: 'text', icon: '../../assets/text.png', placeholder: 'Type Here...', mandatory: false, readOnly: false },
+//     { name: 'Textarea', type: 'textarea', icon: '../../assets/text desc.png', placeholder: 'Type Here...', mandatory: false, readOnly: false },
+//     { name: 'Date', type: 'date', icon: '../../assets/calendar.png', placeholder: 'Type Here...', mandatory: false, readOnly: false },
+//     { name: 'Time', type: 'time', icon: '../../assets/clock.png', placeholder: 'Type Here...', mandatory: false, readOnly: false },
+//     { name: 'Date & Time', type: 'datetime-local', icon: '../../assets/date-time.png', mandatory: false, readOnly: false },
+//     { name: 'Dropdown', type: 'select', icon: '../../assets/drop-down.png', placeholder: 'Select an option', mandatory: false, readOnly: false },
+//     { name: 'Radio Button', type: 'radio', icon: '../../assets/radio.png', mandatory: false, readOnly: false },
+//     { name: 'Checkbox', type: 'checkbox', icon: '../../assets/checkbox.png', mandatory: false, readOnly: false },
+//     { name: 'File Upload', type: 'file', icon: '../../assets/calendar.png', mandatory: false, readOnly: false }
+//   ];
+
+//   @ViewChild('rightDrawer') rightDrawer!: MatDrawer;
+//   @ViewChild('elementEditorDrawer') elementEditorDrawer!: MatDrawer;
+//   selectedElement: any;
+
+//   constructor(private commonService: CommonService, private dialog: MatDialog) { }
+
+//   ngOnInit() {
+//     this.commonService.selectedGroup.subscribe(group => {
+//       this.selectedGroup = group;
+//       if (group) {
+//         this.formElements = group.get('formElementsList') as FormArray;
+//       }
+//     });
+//   }
+
+//   drop(event: CdkDragDrop<any[]>) {
+//     console.log("dropped")
+//     moveItemInArray(this.formElements.controls, event.previousIndex, event.currentIndex);
+//     // if (event.previousContainer !== event.container) {
+//     //   const draggedElement = this.formElementsList[event.previousIndex];
+//     //   this.selectedGroup.formElementsList.push({ ...draggedElement, id: Date.now() });
+//     // }
+//   }
+
+//   addFieldElement(type: string) {
+//     if (!this.selectedGroup) return;
+
+//     const newElement = this.fb.group({
+//       type: [type],
+//       label: [`New ${type}`],
+//       value: [''],
+//       required: [false]
+//     });
+
+//     this.formElements.push(newElement);
+//     this.commonService.saveToLocalStorage();
+//   }
+
+//   removeField(index: number) {
+//     this.formElements.removeAt(index);
+//     // this.commonService.saveToLocalStorage();
+//   }
+
+//   openElementEditor(element: any) {
+//     this.elementEditorDrawer.open()
+//     this.selectedElement = element;
+//   }
+
+//   closeEditor() {
+//     this.elementEditorDrawer.close()
+//   }
+
+//   copyElement(element: any) {
+//     this.selectedGroup.formElementsList.push(element)
+//   }
+
+//   openDeleteDialog(element: any, type: string, index?: number) {
+//     let title = element.name || element.groupName;
+//     const dialogRef = this.dialog.open(ConfirmationPopupComponent, {
+//       width: 'auto',
+//       data: { name: title }
+//     });
+//     dialogRef.afterClosed().subscribe(result => {
+//       if (result && type == 'deleteElement') {
+//         this.selectedGroup.formElementsList.splice(index, 1);
+//       }
+//       else if (result && type == 'deleteGroup') {
+//         console.log("this", this.selectedGroup)
+//         this.selectedGroup = null
+//       }
+//     });
+//   }
+
+//   copyGroup(group: any) {
+//     console.log("group", group)
+//     this.commonService.copiedGroup.next(group)
+//   }
+
+//   saveForm() {
+//     this.commonService.saveToLocalStorage();
+//   }
+
+//   ngOnDestroy() {
+//     this.commonService.selectedGroup.unsubscribe();
+//   }
+// }
+
+
 import { Component, EventEmitter, Output, ViewChild } from '@angular/core';
 import { CommonService } from '../common.service';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon';
 import { DragDropModule, CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 import { MatDrawer } from '@angular/material/sidenav';
@@ -9,6 +142,7 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { LeftPaneComponent } from '../left-pane/left-pane.component';
 import { MatDialog } from '@angular/material/dialog';
 import { ConfirmationPopupComponent } from '../confirmation-popup/confirmation-popup.component';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
 
 @Component({
   selector: 'app-main-frame',
@@ -19,14 +153,18 @@ import { ConfirmationPopupComponent } from '../confirmation-popup/confirmation-p
     MatIconModule,
     DragDropModule,
     MatSidenavModule,
-    LeftPaneComponent
+    LeftPaneComponent,
+    ReactiveFormsModule,
+    MatSlideToggleModule
   ],
   templateUrl: './main-frame.component.html',
   styleUrls: ['./main-frame.component.css']
 })
 export class MainFrameComponent {
-  selectedGroup: any;
   editEnabled: boolean = false;
+  selectedGroup!: FormGroup | null;
+  formElements!: FormArray<FormGroup>;
+  isPreviewMode: boolean = false;
 
   formElementsList = [
     { name: 'Text Field', type: 'text', icon: '../../assets/text.png', placeholder: 'Type Here...', mandatory: false, readOnly: false },
@@ -44,34 +182,53 @@ export class MainFrameComponent {
   @ViewChild('elementEditorDrawer') elementEditorDrawer!: MatDrawer;
   selectedElement: any;
 
-  constructor(private commonService: CommonService, private dialog: MatDialog) { }
+  constructor(private commonService: CommonService, private dialog: MatDialog, private fb: FormBuilder) { }
 
   ngOnInit() {
-    this.commonService.selectedGroup.subscribe(res => {
-      this.selectedGroup = res;
+    this.commonService.selectedGroup.subscribe(group => {
+      this.selectedGroup = group;
+      if (group) {
+        this.formElements = group.get('formElementsList') as FormArray;
+      }
     });
   }
 
   drop(event: CdkDragDrop<any[]>) {
-    console.log("dropped")
-    moveItemInArray(this.selectedGroup.formElementsList, event.previousIndex, event.currentIndex);
+    console.log("dropped");
+    moveItemInArray(this.formElements.controls, event.previousIndex, event.currentIndex);
     if (event.previousContainer !== event.container) {
-      const draggedElement = this.formElementsList[event.previousIndex];
-      this.selectedGroup.formElementsList.push({ ...draggedElement, id: Date.now() });
+        const draggedElement = this.formElementsList[event.previousIndex];
+        this.formElements.push(this.createFormGroup(draggedElement));
     }
   }
 
+  createFormGroup(element:any) {
+    return this.fb.group({
+      name: [element.name],
+      type: [element.type],
+      icon: [element.icon],
+      placeholder: [element.placeholder],
+      mandatory: [element.mandatory],
+      readOnly: [element.readOnly]
+    })
+  }
+
+  removeField(index: number) {
+    this.formElements.removeAt(index);
+  }
+
   openElementEditor(element: any) {
-    this.elementEditorDrawer.open()
+    this.elementEditorDrawer.open();
     this.selectedElement = element;
   }
 
   closeEditor() {
-    this.elementEditorDrawer.close()
+    this.elementEditorDrawer.close();
   }
 
   copyElement(element: any) {
-    this.selectedGroup.formElementsList.push(element)
+    const copiedElement = this.createFormGroup(element)
+    this.formElements.push(copiedElement);
   }
 
   openDeleteDialog(element: any, type: string, index?: number) {
@@ -81,26 +238,32 @@ export class MainFrameComponent {
       data: { name: title }
     });
     dialogRef.afterClosed().subscribe(result => {
-      if (result && type == 'deleteElement') {
-        this.selectedGroup.formElementsList.splice(index, 1);
+      if (result && type === 'deleteElement' && typeof index === 'number') {
+        this.removeField(index)
       }
-      else if (result && type == 'deleteGroup') {
-        console.log("this", this.selectedGroup)
-        this.selectedGroup = null
+      else if (type === 'deleteGroup' && this.selectedGroup) {
+        this.selectedGroup.patchValue({ selected: false });
+        this.selectedGroup = null;
       }
     });
   }
 
   copyGroup(group: any) {
-    console.log("group", group)
-    this.commonService.copiedGroup.next(group)
-  }
-
-  saveForm() {
-    this.commonService.saveToLocalStorage();
+    let formElements = group.formElementsList.map((element: any) => this.createFormGroup(element));
+    const copiedGroup = this.fb.group({
+      id: [Date.now()],
+      groupName: [`${group.groupName} Copy`],
+      groupDesc: [group.groupDesc],
+      selected: [false],
+      formElementsList: this.fb.array(formElements)
+    });
+    console.log("copiedGroup", copiedGroup);
+    this.commonService.copiedGroup.next(copiedGroup);
   }
 
   ngOnDestroy() {
     this.commonService.selectedGroup.unsubscribe();
   }
 }
+
+
